865a5ce530e2edd50a970a0eb829383c695e4fbe
Dan Dees <dand@appsmiths.com>
2017-11-17 10:05:21 +0700

    have fmt::print flush the buffer print error messages on stream fail

--- fmt/ostream.cc
+++ fmt/ostream.cc
@@ -9,6 +9,11 @@
 
 #include "ostream.h"
 
+#if defined( _WIN64 ) || defined( _WIN32 )
+#include <windows.h>
+#endif // defined( _WIN64 ) || defined( _WIN32 )
+#include <sstream>
+
 namespace fmt {
 
 namespace internal {
@@ -21,10 +26,19 @@ FMT_FUNC void write(std::ostream &os, Writer &w) {
   do {
     UnsignedStreamSize n = size <= max_size ? size : max_size;
     os.write(data, static_cast<std::streamsize>(n));
+    if ( os.fail() ) {
+       std::ostringstream ostr ;
+       ostr << "write failed" ;
+#if defined( _WIN64 ) || defined( _WIN32 )
+       ostr << " " << GetLastError() << ": " << strerror(GetLastError());
+#endif // defined( _WIN64 ) || defined( _WIN32 )
+       throw std::runtime_error(ostr.str());
+    }
     data += n;
     size -= n;
   } while (size != 0);
-}
+  os.flush() ; // need to get stuff printed...
+} // write()
 }
 
 FMT_FUNC void print(std::ostream &os, CStringRef format_str, ArgList args) {
